e4d2a7e0070bb4e2343d7afd0bc4688a39c84707
santoshsibyala <santoshsibyala@gmail.com>
2019-12-12 14:04:37 -0800

    seperated pet component from app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-const MyApp = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
-  ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement('MyApp', {
+    React.createElement(Pet, {
       name: 'Luna',
       animal: 'Dog',
       breed: 'Havanese'
     }),
-    React.createElement('MyApp', {
+    React.createElement(Pet, {
       name: 'Pepper',
       animal: 'Bird',
       breed: 'Havanese'
     }),
-    React.createElement('MyApp', {
+    React.createElement(Pet, {
       name: 'Doid',
       animal: 'Cat',
       breed: 'Havanese'
@@ -27,4 +23,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+render(React.createElement(App), document.getElementById('root'));
