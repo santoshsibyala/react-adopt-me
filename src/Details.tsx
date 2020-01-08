ec90a4ea3bbe1bc78894b31e6a05a6b51a1a257b
santoshsibyala <santoshsibyala@gmail.com>
2020-01-08 12:51:00 -0800

    Added typescript to the project

--- src/Details.tsx
+++ src/Details.tsx
@@ -3,14 +3,18 @@ import pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
 import ErrorBounday from './ErrorBoundary';
 import ThemeContext from './ThemeContext';
-import { navigate } from '@reach/router';
+import { navigate, RouteComponentProps } from '@reach/router';
 import Modal from './Modal';
 
-class Details extends React.Component {
-  state = { loading: true };
+class Details extends React.Component<RouteComponentProps<{ id: string }>> {
+  public state = { loading: true };
 
-  componentDidMount() {
-    pet.animal(this.props.id).then(({ animal }) => {
+  public componentDidMount() {
+    if (!this.props.id) {
+      navigate('/');
+      return;
+    }
+    pet.animal(+this.props.id).then(({ animal }) => {
       this.setState({
         url: animal.url,
         name: animal.name,
@@ -24,11 +28,12 @@ class Details extends React.Component {
     });
   }
 
-  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  public toggleModal = () =>
+    this.setState({ showModal: !this.state.showModal });
 
-  adopt = () => navigate(this.state.url);
+  public adopt = () => navigate(this.state.url);
 
-  render() {
+  public render() {
     if (this.state.loading) {
       return <h1>loading ...</h1>;
     }
